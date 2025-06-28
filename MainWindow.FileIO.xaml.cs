991c0bed66460fd67c5f8f2e18ab17ab3c56bc9f
JamesTheButler <ille.tom@outlook.com>
2025-06-28 15:37:03 +0200

    post notifications

--- MainWindow.FileIO.xaml.cs
+++ MainWindow.FileIO.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Diamonds.Utilities;
+﻿using Diamonds.Notification;
+using Diamonds.Utilities;
 using Microsoft.Win32;
 
 namespace Diamonds;
@@ -8,29 +9,42 @@ public partial class MainWindow
     private void QuickSave()
     {
         var saveData = GetSerializedData();
-        
+
         if (_fileManager.ActiveFilePath is not null)
         {
-            FileOperations.Save(saveData, _fileManager.ActiveFilePath);
-            return;
+            var quickSaveResult = FileOperations.Save(saveData, _fileManager.ActiveFilePath);
+
+            if (quickSaveResult)
+            {
+                PostNotification(new SaveSuccessNotification(_fileManager.ActiveFilePath));
+                return;
+            }
         }
 
         var dialog = OpenSaveFileDialog(FileManagementDefaults.FileName, FileManagementDefaults.DefaultLocation);
         if (dialog.ShowDialog() != true)
             return;
 
-        
-        var isSaveSuccess = FileOperations.Save(saveData, dialog.FileName);
+        var fileName = dialog.FileName;
+        var saveResult = FileOperations.Save(saveData, fileName);
 
-        if (isSaveSuccess)
+        if (saveResult)
+        {
             _fileManager.ActiveFilePath = dialog.FileName;
+        }
+
+        INotification notification = saveResult
+            ? new SaveSuccessNotification(fileName)
+            : new SaveFailureNotification(fileName);
+
+        PostNotification(notification);
     }
 
     private SerializedData GetSerializedData()
     {
         return new SerializedData(_currentSizeSettings, _currentColorSettings, _currentDisplaySettings);
     }
-    
+
     private static SaveFileDialog OpenSaveFileDialog(string fileName, string filePath)
     {
         const string extension = FileManagementDefaults.FileExtension;
@@ -56,10 +70,19 @@ public partial class MainWindow
             return;
 
         var saveData = GetSerializedData();
-        var isSaveSuccess = FileOperations.Save(saveData, dialog.FileName);
+        var fileName = dialog.FileName;
+        var saveResult = FileOperations.Save(saveData, fileName);
 
-        if (isSaveSuccess)
+        if (saveResult)
+        {
             _fileManager.ActiveFilePath = dialog.FileName;
+        }
+
+        INotification notification = saveResult
+            ? new SaveSuccessNotification(fileName)
+            : new SaveFailureNotification(fileName);
+
+        PostNotification(notification);
     }
 
     private void LoadFromFile()
@@ -77,30 +100,38 @@ public partial class MainWindow
         if (dialog.ShowDialog() != true)
             return;
 
-        LoadFile(dialog.FileName);
+        var fileName = dialog.FileName;
+        var loadResult = LoadFile(fileName);
+
+        INotification notification = loadResult
+            ? new LoadSuccessNotification(fileName)
+            : new LoadFailureNotification(fileName);
+
+        PostNotification(notification);
 
         ReDraw();
     }
 
-    private void LoadFile(string filePath)
+    private bool LoadFile(string filePath)
     {
         var data = FileOperations.Load(filePath);
         if (data is null)
         {
             Console.WriteLine($"Failed to load or parse '{filePath}'");
-            return;
+            return false;
         }
 
         _fileManager.ActiveFilePath = filePath;
         _currentSizeSettings = data.Value.SizeSettings;
         _currentColorSettings = data.Value.ColorSettings;
         _currentDisplaySettings = data.Value.DisplaySettings;
-        
+
         UnbindInputs();
         RefreshColorInputs();
         RefreshDimensionInputs();
         RefreshDisplayInputs();
         BindInputs();
+        return true;
     }
 
     private void SaveToPng()
@@ -120,5 +151,6 @@ public partial class MainWindow
             return;
 
         FileOperations.SaveAsPng(MainCanvas, dialog.FileName);
+        PostNotification(new SaveSuccessNotification(dialog.FileName));
     }
 }
